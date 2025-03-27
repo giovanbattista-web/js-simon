e80128d49a19ba304264013d34da1d04e9c86fa7
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-27 16:30:29 +0100

    [UPD] PRENDO GLI ELEMENTI DAL DOM

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let numeriCasuali = document.getElementById("number-list");
+let contoAllaRovescia = document.getElementById("countdown");
+/*
+let secondi = 30;
+console.log(contoAllaRovescia);
+contoAllaRovescia.innerText = secondi;  
+console.log(secondi);
+
+const intervallo = setInterval(function () {
+    if (secondi!=0){
+        contoAllaRovescia.innerText = secondi;
+    }
+    secondi--;
+
+}, 1000);
+*/
+
